e70ec40dcec3cd3595fa13f2d6ca471d6aecabab
muskmusk-IT <dokimasiaa12@gmail.com>
2025-08-25 15:52:49 +0700

    Update add_expense.py

--- data/scripts/add_expense.py
+++ data/scripts/add_expense.py
@@ -1,22 +1,3 @@
-import csv
-import sys
-from datetime import datetime
-
-"""
-Cara pakai:
-python scripts/add_expense.py "2025-08-25" "Kopi Kenangan" "Makan & Minum" 32000
-"""
-
-# Ambil argumen dari terminal
-tanggal = sys.argv[1]
-toko = sys.argv[2]
-kategori = sys.argv[3]
-total = sys.argv[4]
-
-# Simpan ke CSV
-with open("data/expenses.csv", "a", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow([tanggal, toko, kategori, total])
-
-print(f"✅ Data tersimpan: {tanggal}, {toko}, {kategori}, {total}")
-
+2025-06-07,Shell Serang Cikupa-1 TGR,Transportasi,50000
+2025-08-01,Kopi Kenangan,Makan & Minum,32000
+2025-08-02,Grab,Transportasi,25000
